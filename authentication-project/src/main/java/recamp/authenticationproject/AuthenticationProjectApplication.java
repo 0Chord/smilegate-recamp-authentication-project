90d27b57d189a62fa84044bc68989c95a32e1564
0Chord <seaweed.0chord@gmail.com>
2023-12-09 23:49:28 +0900

    feat : add authentication-project

---
+++ authentication-project/src/main/java/recamp/authenticationproject/AuthenticationProjectApplication.java
@@ -0,0 +1,13 @@
+package recamp.authenticationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationProjectApplication.class, args);
+	}
+
+}
